b3f59f098a0a18b9b07510ee8a43b80fce98e94a
Roc Wu <rocwind@gmail.com>
2018-08-14 16:41:26 +0800

    add a simple board for logs to html page

--- h5.js
+++ h5.js
@@ -1,3 +1,16 @@
+// output logs
+var board = document.querySelector('.board');
+var oriLog = console.log;
+var logs = [];
+console.log = function(text) {
+    oriLog(text);
+    logs.push(text);
+    if (logs.length > 5) {
+        logs.shift();
+    }
+
+    board.innerHTML = logs.join('<br>');
+}
 
 var canvas = document.querySelector('canvas');
 PerfTest.start({
